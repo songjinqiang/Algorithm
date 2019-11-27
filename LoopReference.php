e301054defe58c5818c265994c2024f6fe036f42
songjq1 <songjq1@ifeng.com>
2019-11-27 11:46:12 +0800

    循环引用

---
+++ LoopReference.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 循环引用 无法释放 进程借宿或缓冲区超过10000释放
+ * User: songjq
+ * Date: 2019/11/7
+ * Time: 16:54
+ */
+$arr=[1,2,3];
+foreach($arr as &$v){
+
+}
+//$v = 'ssss';
+foreach($arr as $v){
+    var_dump($arr);
+}
+print_r($arr);
